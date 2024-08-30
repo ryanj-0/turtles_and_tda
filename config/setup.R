c908dc6d9e957a83180a6bf86cd81d634f8b3058
rzj1019 <johnson.ryan1019@gmail.com>
2024-08-30 15:45:23 -0400

    rewrite needed dirs for new project strct

--- config/setup.R
+++ config/setup.R
@@ -1,12 +1,6 @@
 # set directories
-if(dir != getwd()){
-    dir <- getwd()
-}
-
-data.dir <- paste0(dir, "/data")
-fxn.dir <- paste0(dir, "/functions")
-plots.dir <- paste0(dir, "/plots")
-scripts.dir <- paste0(dir, "/rx")
+data.dir <- paste(getwd(), "data", sep = "/")
+src.dir <- paste(getwd(), "src", sep = "/")
 
 # Set Globals
 my.colors <- c("#0072B2","#E69F00", "#71CCFF", "#F0E442", "#CC79A7")
@@ -14,4 +8,5 @@ my.colors <- c("#0072B2","#E69F00", "#71CCFF", "#F0E442", "#CC79A7")
 #source needed fxns, files, and pkgs
 source(paste0(scripts.dir,"/load_pkgs.R"))
 source(paste0(scripts.dir,"/personals.R"))
+
 
