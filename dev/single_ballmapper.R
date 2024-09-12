47fd874cbb19400eb4f436937f0be94836052389
rzj1019 <johnson.ryan1019@gmail.com>
2024-09-12 01:20:35 -0400

    got single_ballmapper working

--- dev/single_ballmapper.R
+++ dev/single_ballmapper.R
@@ -1,25 +1,25 @@
 single_ballmapper <- function(pointcloud,
                               coloring,
-                              epsilon,
-                              return_bm = TRUE,
-                              return_plot = TRUE) {
+                              epsilon) {
     # Error checking, TBD
 
     # BallMapper Function
-    single.bm <- BallMapper::BallMapper(
-        points = pointcloud,
-        values = coloring,
-        epsilon = epsilon)
-
-    if(return_bm) assign("single.bm", value = single.bm, envir = .GlobalEnv)
+    single.bm <- BallMapper(points = pointcloud,
+                            values = coloring,
+                            epsilon = epsilon)
 
     # Graphing
-    single.bm.plot <- BallMapper::ColorIgraphPlot(
-        outputFromBallMapper = single.bm,
-        seed_for_plotting = 27)
-    title(main = paste("Pointcloud:",
-                       paste(names(pointcloud), collapse = ' | '),
-                       sep = ' '),
-          sub = paste("Epsilon:", epsilon, sep = ' '))
-    if(return_plot) print(single.bm.plot)
+    single.bm.plot <- ColorIgraphPlot(outputFromBallMapper = single.bm,
+                                      seed_for_plotting = 27)
+    title(
+        main = paste("Chemical:", chemical,
+                     "\nEpsilon:", epsilon,
+                     "\nColoring:", coloring.variable,
+                     sep = ' '),
+
+        sub = paste("Pointcloud:",
+                    paste(names(pointcloud), collapse = ' | '),
+                    sep = ' ')
+        )
+
 }
