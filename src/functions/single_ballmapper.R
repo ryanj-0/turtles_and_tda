903e97c8340758ad861bae926206c54cf92f1460
rzj1019 <johnson.ryan1019@gmail.com>
2024-09-12 13:07:48 -0400

    added save file option

--- src/functions/single_ballmapper.R
+++ src/functions/single_ballmapper.R
@@ -3,12 +3,25 @@ single_ballmapper <- function(pointcloud,
                               epsilon) {
     # Error checking, TBD
 
+    # Set locals
+    if(water.temp |> length() > 1) water.temp <- "all"
+
     # BallMapper Function
     single.bm <- BallMapper(points = pointcloud,
                             values = coloring,
                             epsilon = epsilon)
 
     # Graphing
+    if(final == 1){
+        pdf(paste(getwd(), "results/final/bm",
+                  paste(chemical, water.temp, ".pdf", sep = '_'),
+                  sep = '/'))
+    } else {
+        pdf(paste(getwd(), "results/test/bm",
+                  paste(chemical, water.temp, ".pdf", sep = '_'),
+                  sep = '/'))
+        }
+
     bm.plot <- ColorIgraphPlot(outputFromBallMapper = single.bm,
                                seed_for_plotting = 27)
     title(
@@ -23,5 +36,5 @@ single_ballmapper <- function(pointcloud,
                     sep = ' ')
         )
 
-    return(single.bm)
+    dev.off()
 }
