66f698b781db1c345b2e8538c35de7f6b1c44fc5
rzj1019 <johnson.ryan1019@gmail.com>
2024-09-12 01:20:02 -0400

    removed setting dir

--- config/01_setup.R
+++ config/01_setup.R
@@ -1,9 +1,6 @@
 # set directories
-data.dir <- paste(getwd(), "data", sep = '/')
-src.dir <- paste(getwd(), "src", sep = '/')
-results.dir <- paste(getwd(), "results", sep = '/')
 
 # Set Globals
 my.colors <- c("#0072B2","#E69F00", "#71CCFF", "#F0E442", "#CC79A7")
-plasma.colors <- c("#0d0887","#5302a3", "#8b0aa5", "#b83289",
-                   "#db5c68","#f48849", "#febd2a", "#f0f921")
+plasma.colors <- c("#0d0887", "#5302a3", "#8b0aa5", "#b83289",
+                   "#db5c68", "#f48849", "#febd2a", "#f0f921")
