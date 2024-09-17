1169227599a6cecc4e5b7f06227c9dfb4600fa73
rzj1019 <johnson.ryan1019@gmail.com>
2024-09-17 14:33:49 -0400

    broke code into seperate scripts so can run sections if needed

--- src/pipeline/run_bm.R
+++ src/pipeline/run_bm.R
@@ -1,53 +1,10 @@
 ###############################
 # Run full BallMapper pipeline
 ###############################
-#globals
-final <- 0 # place in results/final
-
-# Function Load -----------------------------------------------------------
-source(paste(getwd(), "src/functions/single_ballmapper.R", sep = '/'))
 
 # Single BallMapper -------------------------------------------------------
-# parameters
-c.value <- 0
-chemical <- "hist"
-water.temp <- "21CN"
-coloring.variable <- "water.cont"
-
-# check for args
-if(chemical == "all") {chemical <- ratio.data[, chem] |> unique()
-} else if(!chemical %in% ratio.data[, chem] |> unique()) {
-    stop(paste(chemical, "is not foudn in data", sep = ' '))
-    }
-
-if(water.temp == "all") {water.temp <- ratio.data[, water] |> unique()
-} else if(!water.temp %in% ratio.data[, water] |> unique()) {
-    stop(paste(water.temp, "is not foudn in data", sep = ' '))
-    }
-
-# point cloud
-pc <- ratio.data[correction == c.value &
-                     chem %in% chemical &
-                     water %in% water.temp,
-                 .(pct.zero.ca, pct.delta.prior)]
-
-# coloring varialbe
-c <- ratio.data[correction == c.value &
-                    chem %in% chemical &
-                    water %in% water.temp,
-                .(parse(text = coloring.variable) |>
-                      eval())]
-
-# epsilon
-e <- 0.15
-
-single_ballmapper(pointcloud = pc, coloring = c,
-                  epsilon = e, return_bm = TRUE) |> system.time()
+source(paste(getwd(), "src/pipeline/bm/run_single_bm.R"))
 
 
 # BM Graphs Loop ----------------------------------------------------------
-c.list <- ls()[ls() %like% "cc."]
-source(paste0(fxn.dir, "/set_pointcloud_coloring.R"))
-source(paste0(fxn.dir, "/loop_bm_graphs.R"))
-
-foreach(i=c.list) %do% loop_bm_graphs(i)
+source(paste(getwd(), "src/pipeline/bm/run_bm_loop.R"))
